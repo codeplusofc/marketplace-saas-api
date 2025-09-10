056eac24e1113d68610c3d8944555263fe1c9411
GuiRuchell <ruchellguilherme100@gmail.com>
2025-09-09 22:33:05 -0300

    feat: creating product structure

---
+++ src/main/java/com/market/saas/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.market.saas.repository;
+
+import com.market.saas.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
