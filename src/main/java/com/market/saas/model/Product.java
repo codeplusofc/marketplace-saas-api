92a5c7e34e77e51a9cb3affe8aacc9f847542fd6
Brunoosilva05 <bruninhopf3@gmail.com>
2025-09-16 21:49:55 -0300

    criando classe produto

---
+++ src/main/java/com/market/saas/model/Product.java
@@ -0,0 +1,50 @@
+package com.market.saas.model;
+//id, nome, preço, descrição, vendedorId
+public class Product {
+
+    private Long id;
+    private String nome;
+    private double preco;
+    private String descricao;
+    private Long vendedorId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Long getVendedorId() {
+        return vendedorId;
+    }
+
+    public void setVendedorId(Long vendedorId) {
+        this.vendedorId = vendedorId;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
