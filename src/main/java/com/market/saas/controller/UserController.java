1cf16f12cab99c7a1ae72d2f5a5261fa8353e571
GuiRuchell <ruchellguilherme100@gmail.com>
2025-10-28 21:57:09 -0300

    feat: adding improvements on the user layer

--- src/main/java/com/market/saas/controller/UserController.java
+++ src/main/java/com/market/saas/controller/UserController.java
@@ -1,25 +1,24 @@
 package com.market.saas.controller;
 
+import com.market.saas.model.UserEntity;
 import com.market.saas.service.UserService;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
     private UserService userService;
 
     @GetMapping("/{id}")
-    public User buscarUsuarioPorId(@PathVariable Long id){
-        return userService.User(id);
+    public Optional<UserEntity> buscarUsuarioPorId(@PathVariable Long id){
+        return userService.findUserById(id);
     }
-
-
-
-
-
 }
--- src/main/java/com/market/saas/service/UserService.java
+++ src/main/java/com/market/saas/service/UserService.java
@@ -1,18 +1,19 @@
 package com.market.saas.service;
 
+import com.market.saas.model.UserEntity;
 import com.market.saas.repository.UserRepository;
-import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
     private UserRepository userRepository;
 
-    public User User (long id){
-        Optional<User> user;
-        user = userRepository.findAll(id);
-        return null;
+    public Optional<UserEntity> findUserById(Long id){
+        return userRepository.findById(id);
     }
 }
