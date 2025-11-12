fdeaa93f55fb9503f5383f401e9edf0e18fadf59
Werner Saboia <wernersaboia@gmail.com>
2025-11-12 09:54:55 -0300

    feat: changing orders layers and adding CRUD feature

--- src/main/java/com/market/saas/service/OrderService.java
+++ src/main/java/com/market/saas/service/OrderService.java
@@ -4,6 +4,8 @@ import com.market.saas.model.OrderEntity;
 import com.market.saas.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,17 +18,28 @@ public class OrderService {
     }
 
     public OrderEntity createOrder(OrderEntity orderEntity) {
-        return orderRepository.save(orderEntity);
+        OrderEntity newOrder = new OrderEntity();
+        newOrder.setUserId(orderEntity.getUserId());
+        newOrder.setStatus("PENDING");
+        newOrder.setCreatedAt(LocalDateTime.now());
+
+        return orderRepository.save(newOrder);
     }
 
-    public OrderEntity getAllOrders() {
-        return (OrderEntity) orderRepository.findAll();
+    public List<OrderEntity> getAllOrders() {
+        return orderRepository.findAll();
     }
+
 
     public void deleteOrderById(Long id) {
-        if (orderRepository.existsById(id)) {
-            throw new RuntimeException("Pedido com ID " + id + " não encontrado!");
+        Optional<OrderEntity> pedido = orderRepository.findById(id);
+        if (pedido.isEmpty()) {
+            throw new RuntimeException("Pedido com ID! " + id + " não encontrado");
         }
+        if (!"PENDING".equals(pedido.get().getStatus())) {
+            throw new RuntimeException("Pedido não pode ser deletado!");
+        }
+
         orderRepository.deleteById(id);
     }
 
@@ -39,15 +52,16 @@ public class OrderService {
 
         pedidoDoBanco.get().setUserId(orderEntity.getUserId());
         pedidoDoBanco.get().setStatus(orderEntity.getStatus());
-        pedidoDoBanco.get().setCreatedAt(orderEntity.getCreatedAt());
-        pedidoDoBanco.get().setDescricao(orderEntity.getDescription());
+        pedidoDoBanco.get().setDescription(orderEntity.getDescription());
 
         return orderRepository.save(pedidoDoBanco.get());
     }
 
-    public Optional<OrderEntity> findOrderById(Long id) {
-        return orderRepository.findById(id);
+    public OrderEntity findOrderById(Long id) {
+        Optional<OrderEntity> pedido = orderRepository.findById(id);
+        if (pedido.isEmpty()) {
+            throw new RuntimeException("Pedido com ID " + id + " não encontrado!");
+        }
+        return pedido.get();
     }
-
-
 }
