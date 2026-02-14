a4e7f47a6163d0f901612b0aac252d00adc0313e
GuiRuchell <ruchellguilherme100@gmail.com>
2026-02-14 11:55:28 -0300

    feat:implementing functions on the order layer

--- src/main/java/com/market/saas/controller/OrderController.java
+++ src/main/java/com/market/saas/controller/OrderController.java
@@ -6,10 +6,9 @@ import com.market.saas.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -19,7 +18,22 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<OrderEntity> criar(@RequestBody OrderEntity orderEntity) {
+    public ResponseEntity<OrderEntity> createOrder(@RequestBody OrderEntity orderEntity) {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(orderEntity));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderEntity>> getAllOrders() {
+        return ResponseEntity.status(200).body(orderService.getAllOrders());
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrderById(id);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<OrderEntity> updateOrder(@RequestBody OrderEntity orderEntity, @PathVariable Long id) {
+        return ResponseEntity.status(200).body(orderService.updateOrder(orderEntity, id));
     }
 }
